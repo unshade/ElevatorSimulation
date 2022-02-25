4831c0a1cd6f5f413b49816ceabfe97029dd1e70
unshade <72690981+unshade@users.noreply.github.com>
2022-02-25 09:10:44 +0100

    output 19

--- src/EvenementPassageCabinePalier.java
+++ src/EvenementPassageCabinePalier.java
@@ -32,7 +32,7 @@ public class EvenementPassageCabinePalier extends Evenement {
         Etage etg = null;
 
         //Si les passagers veulent descendre, dans ce cas faire une ouverture de porte
-        if (cabine.passagersVeulentDescendre()) {
+        if (cabine.passagersVeulentDescendre() || étage.aDesPassagers()) {
             echeancier.ajouter(new EvenementOuverturePorteCabine(date + Global.tempsPourOuvrirOuFermerLesPortes));
         }
         //Sinon regarder si la cabine doit monter / descendre et faire un passage de palier
